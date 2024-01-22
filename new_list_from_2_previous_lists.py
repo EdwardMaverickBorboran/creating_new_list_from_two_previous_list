2c665b687c62df1486aaf627b35b8d97057d1bbc
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 01:07:04 +0800

    I added the given set of lists and then also added an empty results list

---
+++ new_list_from_2_previous_lists.py
@@ -0,0 +1,26 @@
+# Create a program that will --
+
+# Create a new list from two list using the following condition
+
+# Given two list of numbers, write a program to create a new list such that the new list- 
+# should contain odd numbers from the first list and even numbers from the second list.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Given:
+# list1 = [10, 20, 25, 30, 35]
+# list2 = [40, 45, 60, 75, 90]
+
+# Expected results:
+# result list: [25, 35, 40, 60, 90]
+
+# 2 given list of numbers
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+
+# Creating an empty list
+results_list = []
+
+
